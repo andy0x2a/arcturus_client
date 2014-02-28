833146b0a7c80493132a6fe9b157740b11bc058d
andy0x2a <andy0x2a@gmail.com>
2014-02-28 09:18:35 -0800

    initial client commit

---
+++ script/client.js
@@ -0,0 +1,151 @@
+var base = "http://localhost:8080/"
+var lcn = "arcturus/location/"
+//var url = base + lcn;
+
+
+var remainingRequests = 0;
+var map = {};
+var locationArray = null;
+function get(url, successCallback) {
+
+    var locationArray = [];
+
+
+    $.ajax({
+                url: url,
+                context: document.body
+            }).done(
+            function() {
+                $(this).addClass("done");
+            }).success(successCallback)
+}
+
+
+var showLocations = function(strData, textSatus, jqXHR) {
+//                       $("#msgid").html(strData[0].latitude + ", " + strData[0].longitude);
+//  alert(strData[0].latitude + ", " + strData[0].longitude);
+
+    console.log("LENGTH: " + strData.length)
+    locationArray = strData;
+
+    remainingRequests = locationArray.length;
+    for (var i = 0; i < locationArray.length; i++) {
+
+        var locn = locationArray[i];
+
+        var entriesLink = locn.links[0];
+
+        map[locn.id] = {'location': locn, 'entries': null};
+
+        get(base + entriesLink.url, showEntries);
+
+
+    }
+
+
+};
+
+function showEntries(entries, textSatus, jqXHR) {
+
+    console.log("ENTRIES LENGTH: " + entries.length)
+    remainingRequests--;
+    var hasEntries = false;
+
+
+    if (!!entries[0]) {
+        var locEntry = map[entries[0].locationId];
+        if (!!locEntry) {
+            locEntry['entries'] = entries;
+        }
+        map[entries[0].locationId] = locEntry;
+    }
+
+
+    if (remainingRequests <= 0) {
+
+        for (var i = 0; i < locationArray.length; i++) {
+            var mapVal = map[locationArray[i].id];
+            if (!!mapVal) {
+                console.log("map val found");
+                displayOnMap(mapVal['location'], mapVal['entries']);
+            }
+
+        }
+
+
+    }
+//    for (var i = 0; i < entries.length; i++) {
+//
+//        var entry = entries[i];
+//
+//         $("#data").html("Rating: " + entry.rating);
+//        var contentId = "#content_"+entry.locationId;
+//        var content = $(contentId);
+//        console.log(content);
+//
+//
+//
+//         displayOnMap(locn, entries);
+//
+//    }
+}
+
+
+function displayOnMap(locn, entries) {
+
+    var latLng = new google.maps.LatLng(locn.latitude, locn.longitude);
+
+    // To add the marker to the map, use the 'map' property
+    var marker = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                title:"Here!"
+            });
+
+    var entryString = "";
+    var ctr = 0;
+
+    if (!!entries) {
+
+        for (var i = 0; i < entries.length; i++) {
+            ctr += entries[i].rating;
+        }
+    }
+
+    if (!!entries) {
+
+        entryString += ("<br> Average rating :" + ctr / entries.length);
+    } else {
+        entryString += ("<br> No Ratings Found ");
+    }
+
+    var contentString = '<div id="content_' + locn.id + '">' +
+            '<h1 id="firstHeading" class="firstHeading">Apartment</h1>' +
+            '<div id="bodyContent">' +
+            '<p> ' + locn.address + ':' +
+            entryString +
+
+            '</div>' +
+            '</div>';
+
+    $("#data").html("Creating..." + locn.address);
+    var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+        $("#data").html("You clicked on " + locn.address);
+    });
+}
+function getLocation(map) {
+    this.map = map;
+    get(base + lcn, showLocations);
+//          var latLng = new google.maps.LatLng(49.208589, -123.124713);
+
+
+}
+
+
+
+
+
